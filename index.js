43ce8c7cfc3b90aa72c131e93616573a640fb92f
HarhslaMjn <mahajanh006@gmail.com>
2023-05-28 10:34:39 +0530

    In-built objects

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello ji");
+
+let lastName =  "Mahajan";
+
+
+let firstName = new String('Harshal');
+
+let message = 'This \n is my \' "first" \n Message';
+console.log(message);
+
+// let words = message. split(' ');
+
+// console.log(words);
