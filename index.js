4ec5e2e5e08742c4cc4c27e603781b0d82a2795d
HarhslaMjn <mahajanh006@gmail.com>
2023-06-03 21:40:58 +0530

    In-built Object

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-console.log("Hello ji");
+// In-built Object Math,String,Template literal,Date & Time
 
-let lastName =  "Mahajan";
+// console.log("Hello ji");
+
+// let lastName =  "Mahajan";
 
 
-let firstName = new String('Harshal');
+// let firstName = new String('Harshal');
 
-let message = 'This \n is my \' "first" \n Message';
-console.log(message);
+// let message = 
+// `This  ${lastName},
+// my  first  
+// Message`;
+// console.log(message);
 
 // let words = message. split(' ');
 
 // console.log(words);
+
+let date = new Date();
+
+let date2 = new Date('June 20 1998 07:15');
+
+let date3 = new Date(1998,5,20 ,7);
+
+
+
+console.log(date);
+
+date3.setFullYear(1947);
+
+console.log(date3);
+
+
